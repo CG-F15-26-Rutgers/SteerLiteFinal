317f5fa5413216b20ee86f8345f45bcb53f3dba5
Jake <jdt97@scarletmail.rutgers.edu>
2015-12-08 23:34:21 -0500

    Fixed python file typo

--- A5_run_all_testcases.py
+++ A5_run_all_testcases.py
@@ -6,8 +6,8 @@ from os import system # system() is used to execute shell commands.
 
 testcases = ["plane_egress", "plane_ingress", "crowd_crossing",
              "office-complex", "hallway-four-way-rounded-roundabout",
-             "bottleneck-squeeze.xml", "doorway-two-way",
-             "double-squeeze", "wall-squeeze", "hallway-two-way", "maze"]
+             "bottleneck-squeeze", "doorway-two-way", "double-squeeze",
+			 "wall-squeeze", "hallway-two-way", "maze"]
 
 command = "./steersim -testcase REPLACE -ai sfAI -storesimulation REPLACE-26.rec"
 
